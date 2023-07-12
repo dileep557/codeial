5789de005f2211bb0f740772244bd6617da75c08
dileep <yadavdileep557@gmail.com>
2023-07-12 15:22:08 +0530

    first commit: file structure is created

---
+++ index.js
@@ -0,0 +1,17 @@
+// start the express 
+
+const express= require('express');
+const app= express();
+const port=8000;
+
+
+
+app.listen(port, function(err)
+{
+    if(err)
+    {
+        console.log(`error ocurrs running on port: ${port}`);
+    }
+
+    console.log(` Server succesfully running on port: ${port}`);
+})
